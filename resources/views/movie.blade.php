8eeeff2dff4e47e926ff8c04f0f140e48f8b89c5
geekMe-png <aburaha6860@icloud.com>
2023-12-12 20:36:50 +0900

    first commit

---
+++ resources/views/movie.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite('resources/css/app.css')
+
+        <!-- Styles -->
+        <style>
+            /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */
+        </style>
+    </head>
+    <body class="bg-stone-800">
+        <img src="asset{{ ../public/assets/images/メイドさん.png }}" alt="映せません">
+    </body>
+</html>
